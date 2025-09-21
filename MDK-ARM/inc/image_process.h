a362658b25a89d130df8b0275d9c55f8e7bff1d1
Z-alia <zhw8204073@163.com>
2025-09-21 17:13:31 +0800

    struct change and basic function

--- MDK-ARM/inc/image_process.h
+++ MDK-ARM/inc/image_process.h
@@ -1,12 +1,24 @@
 #ifndef __image_process_h
 #define __image_process_h
 #include "global.h"
+#define Straight_path 1
+#define Big_bend 1
+#define Small_bend 1
+#define Island_loop 1
+#define Crossroads 1
+#define End 1
+#define scan_start 19
+#define scan_end 40
+#define div_detect_row  15
 typedef struct Image
 {    
 	uint16_t output_image[120][188];
     uint16_t original_image[120][188];
     uint8_t x_points[120];
     uint8_t y_points[120];
+    uint8_t x_leftedge[120];
+    uint8_t x_rightedge[120];
+    uint8_t Type_flag;
 } Image;
 extern struct Image image_buf;
 
--- MDK-ARM/src/image_process.c
+++ MDK-ARM/src/image_process.c
@@ -11,6 +11,7 @@
  *         无需额外的图像缓冲区，效率高，内存占用小。
  *         原始图像数据不会被修改。
  */
+
 void image_edge_detection_optimized(Image *image , uint16_t width, uint16_t height)
 {
     
@@ -56,7 +57,7 @@ void image_edge_detection_optimized(Image *image , uint16_t width, uint16_t heig
 }
 */
 }
-//中线拟合
+//中线拟合,输出边缘位置
 void fit_line(Image *image, int width, float *slope, float *intercept) {
     // 存储中线点的数组
     uint8_t x_points[Display_Height];
@@ -64,13 +65,13 @@ void fit_line(Image *image, int width, float *slope, float *intercept) {
     uint8_t num_points = 0;
 
     // 遍历每一行，找到边缘像素的中点
-    for (int y = 19; y < 40; y++) {
+    for (int y = scan_start; y < scan_end; y++) {
         int left_edge = -1;
         int right_edge = -1;
 
         // 从左到右寻找左边缘
         for (int x = 0; x < width; x++) {
-            if (image->output_image[y][x] > 50) { // 阈值判断为边缘
+            if (image->output_image[y][x] == 1) { // 阈值判断为边缘
                 left_edge = x;
                 break;
             }
@@ -78,16 +79,16 @@ void fit_line(Image *image, int width, float *slope, float *intercept) {
 
         // 从右到左寻找右边缘
         for (int x = width - 1; x >= 0; x--) {
-            if (image->output_image[y][x] > 50) { // 阈值判断为边缘
+            if (image->output_image[y][x] == 1) { // 阈值判断为边缘
                 right_edge = x;
                 break;
             }
         }
-
+        image->x_leftedge[y]=left_edge;
+        image->x_rightedge[y]=right_edge;
         // 如果找到了左右边缘，计算中点
         if (left_edge != -1 && right_edge != -1 && right_edge > left_edge) {
             x_points[num_points] = (left_edge + right_edge) / 2;
-            y_points[num_points] = y;
             num_points++;
         }
     } 
@@ -98,3 +99,46 @@ void image_process_init(Image *image)
     image->output_image[120][188]=0;
 }
 Image image_buf;
+//识别赛道类型
+void read_track_type(Image *image, uint16_t height) {
+    uint16_t track_width=0;
+    uint16_t sum=0;
+    for(uint8_t i=0;i<(scan_end-scan_start);i++)
+    {
+        sum+=image->x_rightedge[scan_start+i]-image->x_leftedge[scan_start+i];
+    }
+    track_width=sum/(scan_end-scan_start);
+    if(track_width>50)//参数待调
+    {
+        image->Type_flag=Straight_path;
+    }
+    else if(track_width>30 && track_width<=50)
+    {
+        image->Type_flag=Big_bend;
+    }
+    else if(track_width>20 && track_width<=30)
+    {
+        image->Type_flag=Small_bend;
+    }
+    else if(track_width>10 && track_width<=20)
+    {
+        image->Type_flag=Island_loop;
+    }
+    else if(track_width<=10)
+    {
+        image->Type_flag=Crossroads;
+    }
+    else if(track_width==0)
+    {
+        image->Type_flag=End;
+    }
+}
+//误差值反馈beta
+float calculate_deviation(Image *image) {
+    // 选择一个参考点
+    uint8_t reference_y = div_detect_row; // 图像底部某点
+    // 计算偏差
+    float deviation = image->x_points[reference_y] - 94; // 94为图像中心点横坐标
+
+    return deviation;
+}
